e76cdf78b07f6fc2a509f3a011f1ca78a71cc35d
Paulo B. Carvalho <paulo.bonfleur@gmail.com>
2019-08-20 12:48:26 -0300

    Fixing

--- meutestedjango/wsgi.py
+++ meutestedjango/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'meutestedjango.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mytestdjango.settings')
 
 application = get_wsgi_application()
