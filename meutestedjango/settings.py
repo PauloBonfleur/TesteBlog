cd90b23420d4294d4080fe4e00a394996c3b1211
Paulo B. Carvalho <paulo.bonfleur@gmail.com>
2019-08-21 12:45:10 -0300

    Fixing

--- meutestedjango/settings.py
+++ meutestedjango/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
-
+    'crispy_forms',
 
 ]
 
@@ -122,3 +122,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
